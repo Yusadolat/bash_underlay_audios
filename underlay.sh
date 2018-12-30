2dba5a87e9eca0334a3a7c2d184ded7570493d01
Yusadolat <yusadolat@gmail.com>
2018-12-30 00:40:17 -0500

    looping and output directory

--- underlay.sh
+++ underlay.sh
@@ -1,2 +1,4 @@
-ffmpeg -i underlay.mp3 -i input2.mp3 -filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 output.mp3
+for f in *.mp3; do ffmpeg -i underlay.mp3 -i "$f" -filter_complex amerge -ac 2 -c:a libmp3lame -q:a 4 outdir/"$f";
+done
+
 
